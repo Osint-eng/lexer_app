8bd1d61de492466e00981d5acd554d3d58883f23
P.varshith <pediredlavarshith.24.csd@anits.edu.in>
2026-03-16 20:45:08 +0530

    Update buffer.py

--- buffer.py
+++ buffer.py
@@ -1,11 +1,16 @@
-class InputBuffer:
+ class InputBuffer:
     def __init__(self, text):
         self.text = text
         self.pos = 0
+        self.buffer_output = []
 
     def next_char(self):
         if self.pos < len(self.text):
             ch = self.text[self.pos]
+            self.buffer_output.append(ch)   # store char
             self.pos += 1
             return ch
         return None
+
+    def get_buffer(self):
+        return self.buffer_output
